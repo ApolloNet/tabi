d465c73b68cecb25caf29794ef9031115bd2006c
Alexandre Ronsaut <alex@apollonet.fr>
2020-03-21 15:32:09 +0100

    Add tests for the Footer component

--- tests/unit/footer.spec.js
+++ tests/unit/footer.spec.js
@@ -2,10 +2,28 @@ import { shallowMount } from '@vue/test-utils'
 import Footer from '@/components/Footer.vue'
 
 describe('Footer.vue', () => {
-
+  const searchFor = jest.fn()
   const wrapper = shallowMount(Footer)
+  wrapper.setData({
+    countries: {
+      papua: {id: 1, name: "papua"},
+      kenya: {id: 2, name: "kenya"}
+    }
+  })
+  wrapper.setMethods({
+    searchFor: searchFor
+  })
 
   it('is a Vue instance', () => {
-    expect(true).toBeTruthy()
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('contains footer links', () => {
+    expect(wrapper.findAll('.footer-link').exists()).toBe(true)
+  })
+
+  it('calls searchFor method on .footer-link click', () => {
+    wrapper.findAll('.footer-link a').at(0).trigger('click')
+    expect(searchFor).toBeCalled()
   })
 })
